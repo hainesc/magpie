684e18c75321a29c03e48b93a5d6dd7f760f1a36
Haines Chan <zhinhai@gmail.com>
2019-03-14 13:50:17 +0800

    show different home page based the role of user

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,32 +1,29 @@
-import { Component } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
-      }
+export class HomeComponent implements OnInit {
 
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
-    })
-  );
-  constructor(private breakpointObserver: BreakpointObserver) { }
+  role = (() => {
+    let groups = (JSON.parse(atob(this.cookie.get('BID').split('.')[1]))).groups as Array<string>
+    if (groups.indexOf('hr') > -1) {
+      return 'hr'
+    }
+    if (groups.indexOf('manager') > -1) {
+      return 'manager'
+    }
+    return 'staff'
+  })();
+
+  constructor(
+    private cookie: CookieService
+  ) { }
+
+  ngOnInit() {
+  }
 }
